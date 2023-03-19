f77713f4fc5e530ac1d20df52e27791d46eb7e99
unknown <francisadewale1992@gmail.com>
2023-03-19 20:47:20 +0000

    add spring boot back end

---
+++ Vescoverer-App/backend/vescoverer/src/main/java/com/spring/fullstack/vescoverer/VescovererApplication.java
@@ -0,0 +1,13 @@
+package com.spring.fullstack.vescoverer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VescovererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VescovererApplication.class, args);
+	}
+
+}
---
+++ Vescoverer-App/backend/vescoverer/src/test/java/com/spring/fullstack/vescoverer/VescovererApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.fullstack.vescoverer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VescovererApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
