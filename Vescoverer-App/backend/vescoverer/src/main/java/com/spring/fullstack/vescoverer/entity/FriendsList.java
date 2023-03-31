a2fe9ce31d37bf6056de6a84e4c1bcf2211baecc
unknown <francisadewale1992@gmail.com>
2023-03-31 15:15:42 +0100

    refactor db schema, refactor spring boot rest to match schema

--- Vescoverer-App/backend/vescoverer/src/main/java/com/spring/fullstack/vescoverer/entity/FriendsList.java
+++ Vescoverer-App/backend/vescoverer/src/main/java/com/spring/fullstack/vescoverer/entity/FriendsList.java
@@ -8,7 +8,7 @@ public class FriendsList {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
+    @Column(name="friend_id")
     int id;
 
     @ManyToOne(cascade= {CascadeType.PERSIST, CascadeType.MERGE,
@@ -16,5 +16,46 @@ public class FriendsList {
     @JoinColumn(name="user_id")
     User user;
 
+    @Column(name="friend_user_id")
+    int friendId;
 
+    public FriendsList() {}
+
+    public FriendsList(User user, int friendId) {
+        this.user = user;
+        this.friendId = friendId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getFriendId() {
+        return friendId;
+    }
+
+    public void setFriendId(int friendId) {
+        this.friendId = friendId;
+    }
+
+    @Override
+    public String toString() {
+        return "FriendsList{" +
+                "id=" + id +
+                ", user=" + user +
+                ", friendId=" + friendId +
+                '}';
+    }
 }
